7a29a549b0662395b432d371643673fb999583b9
kumavis <aaron@kumavis.me>
2016-06-25 10:10:16 -0700

    load state from url

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const urlUtil = require('url')
 const extend = require('xtend')
 const createElement = require('virtual-dom/create-element')
 const diff = require('virtual-dom/diff')
@@ -26,9 +27,24 @@ var state = {
 var totalCalls = traceData.calls.length
 var stackDepth = 0
 
+// setup dom
+var tree = render(state)
+var rootNode = createElement(tree)
+document.body.appendChild(rootNode)
+
+// load state from url
+loadStateFromUrl()
+window.addEventListener('hashchange', loadStateFromUrl, false)
+
+function loadStateFromUrl(){
+  selectFrame(parseUrl())
+  state.autoplay = true
+}
+
 // update state and rerender
 setInterval(function(){
   updateStackFrame()
+  updateUrl()
   rerender()
 }, 1000)
 
@@ -47,7 +63,6 @@ function updateStackFrame() {
   }
 
   if (state.autoplay) {
-    location.href = `#stack-item-${state.frameIndex}`
     state.frameIndex++
   }
 }
@@ -55,13 +70,21 @@ function updateStackFrame() {
 function selectFrame(frameIndex){
   state.frameIndex = frameIndex
   state.autoplay = false
-  changeFocus()
   updateStackFrame()
+  updateUrl()
   rerender()
 }
 
-function changeFocus() {
-  location.href = `#stack-item-${state.frameIndex}`
+function updateUrl(){
+  var hashLocation = `#stack-item-${state.frameIndex}`
+  history.replaceState(undefined, undefined, hashLocation)
+}
+
+function parseUrl(){
+  var hashLocationPrefix = '#stack-item-'
+  var hashLocation = urlUtil.parse(location.href).hash
+  var index = parseInt(hashLocation.slice(hashLocationPrefix.length)) || 0
+  return index
 }
 
 function forwardFrame() {
@@ -79,12 +102,6 @@ function toggleAutoplay() {
 function autoplayStatus() {
   return `Autoplay: ${state.autoplay ? ("ENABLED") : ("DISABLED")}`
 }
-
-
-// setup dom
-var tree = render(state)
-var rootNode = createElement(tree)
-document.body.appendChild(rootNode)
 
 function rerender(){
   var newTree = render(state)
